a6b7cd36c69f14df81383288ad15cbc29c47874d
Tukhtarov Anatoly <atu@ciklum.com>
2018-08-02 09:48:21 +0300

    Create empty m-Swift

---
+++ m-Swift/m-Swift/m_Swift.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for m_Swift.
+FOUNDATION_EXPORT double m_SwiftVersionNumber;
+
+//! Project version string for m_Swift.
+FOUNDATION_EXPORT const unsigned char m_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <m_Swift/PublicHeader.h>
+
+
