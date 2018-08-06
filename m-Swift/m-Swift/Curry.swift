887e254b2ecd24657ca39721529c776108494b2f
Tukhtarov Anatoly <atu@ciklum.com>
2018-08-06 15:44:16 +0300

    Rename bind operator to |>>, make functions public

--- m-Swift/m-Swift/Curry.swift
+++ m-Swift/m-Swift/Curry.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-func curry<A, B, C>(_ function: @escaping (A, B) -> C) -> (A) -> (B) -> C {
+public func curry<A, B, C>(_ function: @escaping (A, B) -> C) -> (A) -> (B) -> C {
     return { argA in { argB in function(argA, argB) } }
 }
 
-func curry<A, B, C, D>(_ function: @escaping (A, B, C) -> D) -> (A) -> (B) -> (C) -> D {
+public func curry<A, B, C, D>(_ function: @escaping (A, B, C) -> D) -> (A) -> (B) -> (C) -> D {
     return { argA in { argB in { argC in function(argA, argB, argC) } } }
 }
 
-func curry<A, B, C, D, E>(_ function: @escaping (A, B, C, D) -> E) -> (A) -> (B) -> (C) -> (D) -> E {
+public func curry<A, B, C, D, E>(_ function: @escaping (A, B, C, D) -> E) -> (A) -> (B) -> (C) -> (D) -> E {
     return { argA in { argB in { argC in { argD in function(argA, argB, argC, argD) } } } }
 }
--- m-Swift/m-Swift/FunctionalOptional.swift
+++ m-Swift/m-Swift/FunctionalOptional.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 infix operator <?>: ApplicativePrecedence
-func <?><A, B>(_ function: (A) -> B, arg: A?) -> B? {
+public func <?><A, B>(_ function: (A) -> B, arg: A?) -> B? {
     return arg.map(function)
 }
 
 infix operator <*>: ApplicativePrecedence
-func <*><A, B>(_ transform: ((A) -> B)?, arg: A?) -> B? {
+public func <*><A, B>(_ transform: ((A) -> B)?, arg: A?) -> B? {
     return transform.flatMap { arg.map($0) }
 }
 
-infix operator >->: BindingPrecedence
-func >-><A, B>(_ transform: ((A) -> B?)?, arg: A?) -> B? {
+infix operator |>>: BindingPrecedence
+public func |>><A, B>(_ transform: ((A) -> B?)?, arg: A?) -> B? {
     return transform.flatMap { arg.flatMap($0) }
 }
